e8a70b1ceb0b1ad4de26de5ab34e823a421e789c
harikrishna87 <krishnahari5048@gmail.com>
2025-06-11 21:32:36 +0530

    Back End Code Implemented

--- src/Controller/AuthController.ts
+++ src/Controller/AuthController.ts
@@ -2,64 +2,9 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../Models/Users';
 import sendToken from '../Utils/jwt';
 import dotenv from 'dotenv';
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { IUser } from '../Types';
 
 dotenv.config();
-
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID!,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: `${process.env.BACKEND_URL || 'http://localhost:5000'}/api/auth/google/callback`
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      let user = await User.findOne({ googleId: profile.id });
-
-      if (user) {
-        return done(null, user);
-      }
-
-      user = await User.findOne({ email: profile.emails?.[0]?.value });
-      if (user) {
-        user.googleId = profile.id;
-        user.name = user.name || profile.displayName;
-        if (user.email === process.env.ADMIN_EMAIL) {
-          user.role = 'admin';
-        }
-        await user.save();
-        return done(null, user);
-      }
-
-      const newUser = {
-        name: profile.displayName,
-        email: profile.emails?.[0]?.value,
-        googleId: profile.id,
-        role: profile.emails?.[0]?.value === process.env.ADMIN_EMAIL ? 'admin' : 'user',
-      } as IUser;
-
-      user = await User.create(newUser);
-      done(null, user);
-
-    } catch (error: any) {
-      done(error, undefined);
-    }
-  }
-));
-
-passport.serializeUser((user: any, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (err) {
-        done(err, null);
-    }
-});
 
 const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
